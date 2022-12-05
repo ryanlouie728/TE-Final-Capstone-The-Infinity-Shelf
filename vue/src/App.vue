d00b44364c8c2c8a793d38ac7a7b4e52900b4ef5
Hannah Reed <hannah.marie.725@gmail.com>
2022-12-05 11:09:59 -0500

    beginning of vue

--- vue/src/App.vue
+++ vue/src/App.vue
@@ -1,9 +1,28 @@
 <template>
   <div id="app">
+    <header id="header">
+      <h1>Comic Book Collection</h1>
+    </header>
     <div id="nav">
       <router-link v-bind:to="{ name: 'home' }">Home</router-link>&nbsp;|&nbsp;
+      <router-link v-bind:to="{ name: '' }">All Collections</router-link>&nbsp;|&nbsp;
+      <router-link v-bind:to="{ name: '' }">My Collections</router-link>&nbsp;|&nbsp;
       <router-link v-bind:to="{ name: 'logout' }" v-if="$store.state.token != ''">Logout</router-link>
+      <router-link v-bind:to="{ name: 'login' }" v-if="$store.state.token == ''">Login</router-link>
     </div>
     <router-view />
   </div>
 </template>
+
+<style>
+#nav {
+    margin-top: 0.2em;
+    margin-bottom: 0.2em;
+    padding-top: 0.2em;
+    padding-bottom: 0.2em;
+    background-color: #BFBDB8;
+    border-top: solid 1px black;
+    border-bottom: solid 1px black;
+}
+
+</style>
