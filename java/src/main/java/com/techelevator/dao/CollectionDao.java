c0501798e748893d77f4815bb405d95ca150f0d1
Ben Brauns <braunsben@gmail.com>
2022-12-05 10:31:36 -0600

    added in collection creation and collection updating

--- java/src/main/java/com/techelevator/dao/CollectionDao.java
+++ java/src/main/java/com/techelevator/dao/CollectionDao.java
@@ -6,4 +6,8 @@ import java.util.List;
 
 public interface CollectionDao {
     List<SimpleCollectionDto> listCollectionsByUserId(int userId);
+
+    void createCollection(SimpleCollectionDto collection);
+
+    void updateCollection(SimpleCollectionDto collection);
 }
