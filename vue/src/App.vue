51077b173b2918ab2149799807ea44c21b1a3761
Hannah Reed <hannah.marie.725@gmail.com>
2022-12-07 14:12:45 -0500

    added color variables

--- vue/src/App.vue
+++ vue/src/App.vue
@@ -15,9 +15,18 @@
 </template>
 
 <style>
+:root{
+  --black: #000505;
+  --violet: #3B3355;
+  --navy: #5D5D81;
+  --blue: #BFCDE0;
+  --white: #FEFCFD;
+}
+
 html {
   box-sizing: border-box;
   min-height: 100vh;
+  background-color: var(--white);
 }
 
 body {
@@ -52,9 +61,9 @@ header.h1 {
   padding-top: 0.2em;
   padding-bottom: 0.2em;
   height: fit-content;
-  background-color: #BFBDB8;
-  border-top: solid 1px black;
-  border-bottom: solid 1px black;
+  background-color: var(--blue);
+  border-top: solid 2px var(--black);
+  border-bottom: solid 2px var(--black);
 }
 
 #router-view {
@@ -63,8 +72,8 @@ header.h1 {
 
 #sidebar {
     width: 200px;
-    background-color: lightgray;
-    border: solid 2px darkgray;
+    background-color: var(--blue);
+    border: solid 2px var(--black);
 }
 
 </style>
