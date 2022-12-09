cfe5cb85a32b4cd5574ef8a8d38e355880c9c767
Zachary Smith[D[D[D[D[D[D[C[C[C[C[C[C <zecharious@gmail.com>
2022-12-09 15:33:50 -0500

    CollectionDetails minor formatting

--- vue/src/views/CollectionDetails.vue
+++ vue/src/views/CollectionDetails.vue
@@ -1,103 +1,129 @@
 <template>
   <div id="collection-details">
     <div id="left-pane">
-      <h1>{{this.collection.collectionName}}</h1>
-      <img v-bind:src="this.collection.collectionCoverUrl" alt="" id="cover-image">
-      <h2>Description</h2>
-      <p>{{this.collection.collectionDescription}}</p>
-      <h3>Privacy</h3>
-      <p>This collection is {{collectionPrivacy()}}</p>
-      <div id="update-collection">
-        <button v-on:click="updatingCollection = !updatingCollection">Update Collection</button>
+      <div id="img">
+        <img
+          v-bind:src="this.collection.collectionCoverUrl"
+          alt=""
+          id="cover-image"
+        />
       </div>
-      <update-collection v-if="updatingCollection" v-bind:collection="this.collection" />
-      <remove-collection v-bind:collection="this.collection" />
-      <h2 banner>Comics in the {{this.collection.collectionName}} Collection</h2>
-      <comic-list v-bind:showAdd="true" ref="comics" @clicked="comicClicked()" v-bind:comics="this.collection.comics" @addComic="addingComic = true"/>
+      <div id="desc">
+        <h1>{{ this.collection.collectionName }}</h1>
+        <h2>Description</h2>
+        <p>{{ this.collection.collectionDescription }}</p>
+      </div>
+      <div id="privacy">
+        <h3>Privacy</h3>
+        <p>This collection is {{ collectionPrivacy() }}</p>
+      </div>
+      <div id="update-collection">
+        <button v-on:click="updatingCollection = !updatingCollection">
+          Update Collection
+        </button>
+      </div>
+      <update-collection
+        v-if="updatingCollection"
+        v-bind:collection="this.collection"
+      />
+      <div id="remove-collection">
+        <remove-collection v-bind:collection="this.collection" />
+      </div>
+      <div id="add-comic">
+        <h2 banner>Comics in the {{ this.collection.collectionName }} Collection</h2>
+        <comic-list v-bind:showAdd="true" ref="comics" @clicked="comicClicked()" v-bind:comics="this.collection.comics" @addComic="addingComic = true" />
+        <add-comic v-if="addingComic" @added="comicAdded()" v-bind:collection="this.collection" />
+      </div>
     </div>
     <div id="sidebar">
       <div id="counts">
         <div class="count-list">
           <h5 class="count-title">Top Character Appearances</h5>
-          <div class="count-row" v-for="character in this.collection.characterCounts.slice(0,5)" v-bind:key="character.name">
-            <h6 class="count-name">{{character.name}}</h6>
+          <div
+            class="count-row"
+            v-for="character in this.collection.characterCounts.slice(0, 5)"
+            v-bind:key="character.name"
+          >
+            <h6 class="count-name">{{ character.name }}</h6>
           </div>
         </div>
         <div class="count-list">
           <h5 class="count-title">Top Creator Appearances</h5>
-          <div class="count-row" v-for="creator in this.collection.creatorCounts.slice(0,5)" v-bind:key="creator.name">
-            <h6 class="count-name">{{creator.name}}</h6>
+          <div
+            class="count-row"
+            v-for="creator in this.collection.creatorCounts.slice(0, 5)"
+            v-bind:key="creator.name"
+          >
+            <h6 class="count-name">{{ creator.name }}</h6>
           </div>
         </div>
         <div class="count-list">
           <h5 class="count-title">Collection Description</h5>
           <div class="description-text">
-            <p> {{ collection.collectionDescription }} </p>
+            <p>{{ collection.collectionDescription }}</p>
           </div>
         </div>
       </div>
     </div>
-    <add-comic v-if="addingComic" @added="comicAdded()" v-bind:collection="this.collection" />
-    
   </div>
-
- 
 </template>
 
 <script>
-import ComicList from '../components/ComicList.vue';
-import AddComic from '../components/AddComic.vue';
-import CollectionService from '../services/CollectionService';
-import UpdateCollection from '../components/UpdateCollection.vue';
-import RemoveCollection from '../components/RemoveCollection.vue';
-
+import ComicList from "../components/ComicList.vue";
+import AddComic from "../components/AddComic.vue";
+import CollectionService from "../services/CollectionService";
+import UpdateCollection from "../components/UpdateCollection.vue";
+import RemoveCollection from "../components/RemoveCollection.vue";
 
 export default {
   components: { ComicList, AddComic, UpdateCollection, RemoveCollection },
-  props: ['collection-details'],
-  name: 'collection-details',
+  props: ["collection-details"],
+  name: "collection-details",
   data() {
     return {
       collection: {
         characterCounts: [],
-        creatorCounts: []
+        creatorCounts: [],
       },
       addingComic: false,
       updatingCollection: false,
       removingComic: false,
       selectedComics: [],
-    }
+    };
   },
   methods: {
-    collectionPrivacy(){
-      return this.collection.collectionPublic ? 'Public' : 'Private';
+    collectionPrivacy() {
+      return this.collection.collectionPublic ? "Public" : "Private";
     },
     getCollection() {
-      CollectionService.getCollectionById(this.$route.params.id)
-      .then(response => {
-        if (response.status == 200) {
-          this.collection = response.data
+      CollectionService.getCollectionById(this.$route.params.id).then(
+        (response) => {
+          if (response.status == 200) {
+            this.collection = response.data;
+          }
         }
-      })
+      );
     },
     comicClicked() {
       if (this.removingComic) {
         this.selectedComics.push(this.$refs.comics.clickedId);
         console.log(this.selectedComics);
 
-        CollectionService.removeComicFromCollection(this.collection.collectionId, this.$refs.comics.clickedId)
-        .then(response => {
+        CollectionService.removeComicFromCollection(
+          this.collection.collectionId,
+          this.$refs.comics.clickedId
+        ).then((response) => {
           if (response.status == 200) {
             this.getCollection();
           }
-        })
+        });
       } else {
         this.$router.push({
-          name: 'comic-details',
+          name: "comic-details",
           params: {
-            id: this.$refs.comics.clickedId
-          }
-        })
+            id: this.$refs.comics.clickedId,
+          },
+        });
       }
     },
     comicAdded() {
@@ -107,11 +133,39 @@ export default {
   },
   created() {
     this.getCollection();
-  }
-}
+  },
+};
 </script>
 
 <style>
+#left-pane {
+  display: grid;
+  grid-template-columns: 1fr 1fr 1fr;
+  grid-template-areas:
+    "img desc desc"
+    "img desc desc"
+    "privacy update remove-collection"
+    "add-comic add-comic add-comic";
+}
+#add-comic{
+  grid-area: add-comic;
+}
+#update-collection {
+  grid-area: update;
+}
+#img {
+  grid-area: img;
+}
+#desc {
+  grid-area: desc;
+}
+#privacy {
+  grid-area: privacy;
+}
+#remove-collection {
+  grid-area: remove-collection;
+}
+
 #router-view {
   display: flex;
 }
@@ -138,7 +192,7 @@ export default {
 #counts {
   width: 200px;
 }
-#cover-image{
+#cover-image {
   height: 316px;
   width: 200px;
 }
