80ed5645c258f7c7a9afe17506ce980d68643bed
Ben Brauns <braunsben@gmail.com>
2022-12-10 10:51:37 -0600

    added more data into data.sql

--- java/database/data.sql
+++ java/database/data.sql
@@ -15,4 +15,20 @@ INSERT INTO user_collection (user_id, coll_id) VALUES (3, 3);
 INSERT INTO user_friend (user_id, friend_id) VALUES (1, 3);
 INSERT INTO user_friend (user_id, friend_id) VALUES (3, 1);
 
+INSERT INTO users (username,password_hash,role) VALUES ('braunsben', '$2a$10$6okttCWmnMxktiDBdLzbKOB9xHKxZyyfpJ/UUZwQUGyyZGBMgpJnG', 'ROLE_USER');
+INSERT INTO collection (user_id, coll_name) VALUES (4, 'base');
+INSERT INTO user_collection (user_id, coll_id) VALUES (4, 4);
+
+INSERT INTO users (username,password_hash,role) VALUES ('reedhannah', '$2a$10$6okttCWmnMxktiDBdLzbKOB9xHKxZyyfpJ/UUZwQUGyyZGBMgpJnG', 'ROLE_USER');
+INSERT INTO collection (user_id, coll_name) VALUES (5, 'base');
+INSERT INTO user_collection (user_id, coll_id) VALUES (5, 5);
+
+INSERT INTO users (username,password_hash,role) VALUES ('smithzach', '$2a$10$6okttCWmnMxktiDBdLzbKOB9xHKxZyyfpJ/UUZwQUGyyZGBMgpJnG', 'ROLE_USER');
+INSERT INTO collection (user_id, coll_name) VALUES (6, 'base');
+INSERT INTO user_collection (user_id, coll_id) VALUES (6, 6);
+
+INSERT INTO users (username,password_hash,role) VALUES ('louieryan', '$2a$10$6okttCWmnMxktiDBdLzbKOB9xHKxZyyfpJ/UUZwQUGyyZGBMgpJnG', 'ROLE_USER');
+INSERT INTO collection (user_id, coll_name) VALUES (7, 'base');
+INSERT INTO user_collection (user_id, coll_id) VALUES (7, 7);
+
 COMMIT TRANSACTION;
