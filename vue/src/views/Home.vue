6916d5ce79bce868dff4c4dad3f4e4a91ef11b75
Hannah Reed <hannah.marie.725@gmail.com>
2022-12-12 12:43:55 -0500

    Aggregate Stats work

--- vue/src/views/Home.vue
+++ vue/src/views/Home.vue
@@ -2,34 +2,62 @@
   <div class="home">
     <div id="left-pane">
       <h2>Aggregate Stats</h2>
+      <div class="stat-info">
+        <div class="character">
+          <h3 class="character-title">Characters and their Appearances</h3>
+          <div
+            class="count-row"
+            v-for="character in aggregate.characters.slice(0, 7)"
+            v-bind:key="character.name"
+          >
+            <h4 class="count-name">
+              {{ character.name + ", Appearances: " + character.count }}
+            </h4>
+          </div>
+        </div>
+        <div class="creator">
+          <h3 class="creator-title">Creators and their Appearances</h3>
+          <div
+            class="count-row"
+            v-for="creator in aggregate.creators.slice(0, 7)"
+            v-bind:key="creator.name"
+          >
+            <h4 class="count-name">
+              {{ creator.name + ", Appearances: " + creator.count }}
+            </h4>
+          </div>
+        </div>
+      </div>
     </div>
   </div>
 </template>
 
 <script>
-import CollectionService from '../services/CollectionService';
+import CollectionService from "../services/CollectionService";
 
 export default {
   name: "home",
   data() {
     return {
-      aggergate: {}
-    }
+      aggregate: {
+        creators: [],
+        characters: [],
+      },
+    };
   },
   methods: {
     getAggregate() {
-      CollectionService.getAggregateStats()
-      .then(response => {
+      CollectionService.getAggregateStats().then((response) => {
         if (response.status == 200) {
-          this.aggergate = response.data
-          console.log(this.aggergate)
+          this.aggregate = response.data;
+          console.log(this.aggregate);
         }
-      })
-    }
+      });
+    },
   },
   created() {
     this.getAggregate();
-  }
+  },
 };
 </script>
 
@@ -47,5 +75,14 @@ export default {
   flex-direction: column;
   justify-content: flex-start;
   align-content: flex-start;
+}
+
+.stat-info {
+  display: flex;
+  flex-direction: row;
+  justify-content: flex-start;
+  align-content: flex-start;
+  flex-grow: 1;
+  justify-content: space-around;
 }
 </style>
